a89033f8cc3ad65a7d2a37cac81ab335b1838b56
Deeppjp116 <deeppjp1656@gmail.com>
2025-02-26 10:07:29 +0530

    secound commit

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,20 +1,39 @@
 import express from 'express';
 import { TrainModel, GenerateImage, GenerateImageFromPack } from "common/types"
+import { prismaClient } from "db"
+
 
 
 const app = express();
+app.use(express.json())
 
 const PORT = process.env.PORT || 8080;
 
 
- 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+app.post('/ai/traning', async (req, res) => {
+    const parsedBody = TrainModel.safeParse(req.body);
 
-app.post('/ai/traning', (req, res) => {
-    res.send('Here is a traning!');
+    if (!parsedBody.success) {
+        res.status(411).json({
+            message: 'Invalid incorrect'
+        })
+        return;
+    }
+
+    await prismaClient.model.create({
+        data: {
+            name: parsedBody.data.name,
+            type: parsedBody.data.type,
+            age: parsedBody.data.age,
+            ethinicity: parsedBody.data.ethinicity,
+            eyeColor: parsedBody.data.eyeColor,
+            bald: parsedBody.data.bald,
+        }
+    })
 });
 
 app.post('/ai/generate', (req, res) => {
--- packages/common/types.ts
+++ packages/common/types.ts
@@ -3,9 +3,10 @@ import { z } from "zod";
 
 export const TrainModel = z.object({
     name: z.string(),
-    type: z.enum(["Man", "Woman", "Other"]),
-    ethinicity: z.enum(["White", "Black", "Asian American", "East American", "South East American", "South Asian", "Middel Eastern", "Pacific", "Hispanic", "Other"]),
-    eyeColor: z.enum(["Blue", "Brown", "Green", "Black", "Other"]),
+    type: z.enum(["Man", "Woman", "Others"]),
+    age: z.number(),
+    ethinicity: z.enum(["White", "Black", "Asian_American", "East_Asian", "South_East_Asian", "South_Asian", "Middle_Estern", "Pacific", "Hispanic"]),
+    eyeColor: z.enum(["Brown", "blue", "Hazel", "Gray",]),
     bald: z.boolean(),
     images: z.array(z.string()),
 })
